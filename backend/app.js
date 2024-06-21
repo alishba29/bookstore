601c7b74f54359fe3fd2e5c5c4370c5dcde575ba
alishba29 <khaweralishba@gmail.com>
2024-06-21 20:40:43 +0500

    chore: Add Cart and Order routes to app.js

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,15 @@ require("./conn/conn");
 const User = require("./routes/user");
 const Books = require("./routes/book");
 const Favourite = require("./routes/favourite");
-
+const Cart = require("./routes/cart");
+const Order = require("./routes/order");
 //routes
 app.use(express.json());
 app.use("/api/v1", User);
 app.use("/api/v1", Books);
 app.use("/api/v1", Favourite);
+app.use("/api/v1", Cart);
+app.use("/api/v1", Order);
 // creating port
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
