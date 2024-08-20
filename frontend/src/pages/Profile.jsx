3598f5f47975f50e523e83f8d7fdab64086d471b
alishba29 <khaweralishba@gmail.com>
2024-08-20 12:12:13 +0500

    chore: Update react-icons to version 5.3.0

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+import Sidebar from '../components/Profile/sidebar'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import axios from 'axios'
+import Loader from '../components/Loader/Loader'
 
 const Profile = () => {
+  //const isLoggedIn = useSelector();
+ const [Profile, setProfile] = useState();
+  const headers ={
+    id: localStorage.getItem('id'),
+    authorization: `Bearer ${localStorage.getItem('token')}`,
+  };
+    useEffect(() => {
+      const fetch = async()=>{
+        const response = await axios.get('http://localhost:3000/api/v1/get-user-information',{headers}); 
+        setProfile(response.data);
+      };
+
+      fetch();
+      
+    }, [])
+    
   return (
-    <div>Profile</div>
+    <div className='bg-zinc-900 px-2 md:px-12 flex flex-col md:flex-row gap-4 text-white h-screen py-8'>
+      {!Profile && <div className='w-full h-[100%] flex items-center justify-center'><Loader/></div>
+      }
+      {Profile && (
+        <>
+        <div className="w-full md:w-1/6"><Sidebar data={Profile}/></div>
+        <div className="w-full md:w-5/6"><Outlet/>body</div>
+        </>
+      )}
+    </div>
   )
 }
 
